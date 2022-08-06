7f803b01f77a0cd71e1ae24b23a6d24f4c960eca
Gobinda Chandra <g.chandra.2501@icloud.com>
2022-08-06 10:22:34 +0530

    1

--- 2022/Compact Matrix/__init__.py
+++ 2022/Compact Matrix/__init__.py
@@ -1,10 +1,2 @@
 import pkg_resources
 
-from manim import*
-from animlib import*
-from animlib.const import*
-from animlib.animation.animation import*
-from animlib.animation.creation import*
-from animlib.constructs.graph_scene import*
-from animlib.mobject.functions import*
-from animlib.utils.config_ops import*
